fa19902ac12a819ae8bef34cdaf74042915d8e94
Alex <you@example.com>
2020-05-06 13:32:08 +0300

    Правка содержимого

--- php-sql-tables/blocks/content.php
+++
@@ -1,8 +0,0 @@
-<?php
-		// $path = "./files/test.txt";
-		$path = $_SERVER["DOCUMENT_ROOT"] . "/demo/files/test.txt";
-		$file = fopen($path, "r");
-	while(!feof($file)) {
-		echo fgets($file);
-	}
-?>
